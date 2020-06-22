d5401c14a3ec7c80d9f4dcbe0d2e24cb3d190938
muskan.mangla <muskanmangla70@gmail.com>
2020-06-22 18:22:31 +0530

    Http requests and web scraping done

---
+++ code.py
@@ -0,0 +1,10 @@
+#arithmetic operations
+a=10
+b=3
+print(a//b)    # integer division
+print(a/b)     # floating point division
+
+print("%0.4f"%(a/b))    # precision upto 4 decimals
+
+print(a*b)     # multiplication
+print(a**b)    # power calculation
